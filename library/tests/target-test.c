800778a31229e3a41cca4ae6bef99422d2e4c412
Leonardo Celente <leocelente@gmail.com>
2021-02-26 23:54:32 -0300

    Exemplo de teste da biblioteca

---
+++ library/tests/target-test.c
@@ -0,0 +1,40 @@
+#include "ipc.h"
+#include "target/target.h"
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+
+comm_t gcomm;
+
+int main(int argc, char **argv) {
+	int child_pid = *(int *)argv[0];
+	comm_init(&gcomm, ROLE_PARENT);
+	/* USER CODE BEGIN */
+
+
+	target_t target;
+	target_init(&target, &gcomm);
+
+	target_set_config(&target, MODE_PADRAO);
+
+	char tx[] = { 0xAD, 0xDE };
+	target_send_packet(&target, tx, sizeof(tx));
+
+	char rx[1];
+	target_get_mode(&target, rx, 1);
+	printf("RX: %02X \n", (uint8_t)rx[0]);
+
+
+	/* USER CODE END */
+
+	char _data[COMM_BUFF_SZ] = { 'q' };
+	uint8_t _rx[COMM_BUFF_SZ] = { 0 };
+	comm_write(&gcomm, (uint8_t *)_data, COMM_BUFF_SZ);
+	comm_read(&gcomm, _rx, COMM_BUFF_SZ);
+	puts("Waiting for Child...");
+	wait(NULL);
+	comm_close(&gcomm);
+	puts("End of Parent");
+}
